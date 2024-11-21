13561d5462e25187e56d342ed199dbebec0a4782
NoNathan17 <nathanong1217@gmail.com>
2024-11-20 19:26:20 -0800

    refactored frontend to use vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,38 @@
-import react from 'react'
-import React, { useEffect, useState } from 'react';
+import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/login"
 import Register from "./pages/register"
 import Home from "./pages/home"
 import ProtectedRoute from "./components/ProtectedRoute"
-import logo from './logo.svg';
-import './App.css';
 
 function Logout() {
-    localStorage.clear()
-    return <Navigate to="/login" />
-  }
-  
-  function RegisterAndLogout() {
-    localStorage.clear()
-    return <Register />
-  }
-  
-const App = () => {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <h1>nutri !!</h1>
-        <p>
-          a fitness application designed to help you meet your personal <br />
-          fitness goals. made by jay + nathan + danny
-        </p>
-        <p>{/*message || 'Loading...'*/}</p>
-  
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<RegisterAndLogout />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+      </Routes>
     </BrowserRouter>
-  );  
-};
+  )
+}
 
-export default App;
+export default App
