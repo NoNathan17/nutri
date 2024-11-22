102d036b946b5d6698f30681dee4b3a93e10d600
Jay Chan <jayc10@uci.edu>
2024-11-21 23:28:07 -0800

    bodybuilding plan touches

--- frontend/src/pages/bodybuilding.tsx
+++ frontend/src/pages/bodybuilding.tsx
@@ -7,53 +7,58 @@ import {
 
 const Bodybuilding = () => {
   return (
-    <div style={{ marginTop: '150px', width: '500px'}} className="form-container flex flex-col space-y-5">
-      <h1 style={{ fontSize: '18px' }}>bodybuilding !!</h1>
-      <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger className="flex justify-center w-full">monday</AccordionTrigger>
+    <div style={{ marginTop: '150px', width: '500px', height: '600px', backgroundColor: '#d8e3d3'}} className="form-container flex flex-col space-y-5">
+      <h1 style={{ fontSize: '18px' }}>Bodybuilding Plan !!</h1>
+      <div className="mt-4 text-[#44624a]">
+        <p style={{fontSize: '12px' }}>
+          A plan tailored to build muscle and strength effectively.
+        </p>
+      </div>
+      <Accordion type="single" collapsible className="w-full text-[#44624a]">
+      <AccordionItem value="item-1" className="border-b-[1px] border-[#44624a]">
+        <AccordionTrigger>Monday</AccordionTrigger>
         <AccordionContent>
           Chest and Triceps - Bench Press, Dumbbell Flys, Tricep Dips<br />
           + Grilled Chicken, Quinoa, Broccoli
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>tuesday</AccordionTrigger>
+      <AccordionItem value="item-2" className="border-b-[1px] border-[#44624a]">
+        <AccordionTrigger>Tuesday</AccordionTrigger>
         <AccordionContent>
           Back and Biceps - Pull-Ups, Barbell Rows, Bicep Curls<br />
           + Salmon, Brown Rice, Asparagus
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>wednesday</AccordionTrigger>
+      <AccordionItem value="item-3" className="border-b-[1px] border-[#44624a]">
+        <AccordionTrigger>Wednesday</AccordionTrigger>
         <AccordionContent>
           Shoulders and Abs - Shoulder Press, Lateral Raises, Planks<br />
           + Steak, Sweet Potatoes, Spinach
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-4">
-        <AccordionTrigger>thursday</AccordionTrigger>
+      <AccordionItem value="item-4" className="border-b-[1px] border-[#44624a]">
+        <AccordionTrigger>Thursday</AccordionTrigger>
         <AccordionContent>
           Rest day !<br />
           + Light Salad, Avocado, Nuts
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-5">
-        <AccordionTrigger>friday</AccordionTrigger>
+      <AccordionItem value="item-5" className="border-b-[1px] border-[#44624a]">
+        <AccordionTrigger>Friday</AccordionTrigger>
         <AccordionContent>
           Legs - Squats, Lunges, Leg Press <br />
           + Turkey Breast, Green Beans, Rice
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-6">
-        <AccordionTrigger>saturday</AccordionTrigger>
+      <AccordionItem value="item-6" className="border-b-[1px] border-[#44624a]">
+        <AccordionTrigger>Saturday</AccordionTrigger>
         <AccordionContent>
           Full Body - Deadlifts, Push-Ups, Kettlebell Swings <br />
           + Grilled Shrimp, Couscous, Kale
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-7">
-        <AccordionTrigger>sunday</AccordionTrigger>
+      <AccordionItem value="item-7" className="border-b-[1px] border-[#44624a]">
+        <AccordionTrigger>Sunday</AccordionTrigger>
         <AccordionContent>
           Rest day ! <br />
           + Protein Shake, Mixed Fruit
