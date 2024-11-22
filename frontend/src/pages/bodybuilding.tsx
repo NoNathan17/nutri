1e6ead5356710bdc02f547de280f3b61279096d6
Jay Chan <jayc10@uci.edu>
2024-11-22 15:02:04 -0800

    bodybuilding page

--- frontend/src/pages/bodybuilding.tsx
+++ frontend/src/pages/bodybuilding.tsx
@@ -7,12 +7,45 @@ import {
 } from "@/components/ui/accordion"
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { TrendingUp } from "lucide-react"
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { month: "0.5", calories: 250, protein: 75 },
+  { month: "1.0", calories: 500, protein: 130 },
+  { month: "1.5", calories: 750, protein: 225 },
+  { month: "2.0", calories: 1000, protein: 300 },
+];
+
+const chartConfig = {
+  calories: {
+    label: "Calories",
+    color: "#9caf88",
+  },
+  protein: {
+    label: "Protein",
+    color: "#6e8f6b",
+  },
+} satisfies ChartConfig
 
 const Bodybuilding = () => {
   const [bmi, setBmi] = useState(null);
   const [maintenanceCalories, setMaintenanceCalories] = useState(null);
-  const [loading, setLoading] = useState<boolean>(true); // State to manage loading status
+  const [loading, setLoading] = useState<boolean>(true);
 
   const name = localStorage.getItem('name');
 
@@ -35,89 +68,64 @@ const Bodybuilding = () => {
     <div style={{ marginTop: '150px', width: '1000px', height: 'auto', backgroundColor: '#d8e3d3'}} className="form-container flex flex-col space-y-5">
     <h1 style={{ fontSize: '18px', marginTop: '15px' }}>Bodybuilding Plan</h1>
       <p style={{fontSize: '12px', marginTop: '0px' }}>
-        A plan tailored to build muscle and strength effectively!
+        A plan tailored to build muscle and strength effectively!<br />
+        BMI: {bmi ? bmi : "No Data Available"} | Maintenance Calories: {maintenanceCalories ? maintenanceCalories : "No Data Available"}
       </p>
-    <div>
-      {loading ? (<div></div>
-          
-        ) : (
-            <div className="mt-4 text-[#44624a]">
-              <h1 style={{ fontSize: '30px' }}>{ name }</h1>
-              <h2>BMI: {bmi ? bmi : "No Data Available"}</h2>
-              <h2>Given your maintenance calorie count of {maintenanceCalories ? maintenanceCalories : "No Data Available"}</h2>
-              <Table
-  style={{
-    marginTop: "25px",
-    width: "1000px",
-    backgroundColor: "#d8e3d3",
-    borderRadius: "10px",
-    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-  }}
->
-  <TableHeader>
-    <TableRow>
-      <TableHead
-        style={{
-          color: "#44624a",
-          textAlign: "center",
-        }}
-      >
-        Calories Consumed/Day
-      </TableHead>
-      <TableHead
-        style={{
-          color: "#44624a",
-          textAlign: "center",
-        }}
-      >
-        Muscle Gained(lbs)/Week
-      </TableHead>
-    </TableRow>
-  </TableHeader>
-  <TableBody>
-    <TableRow>
-      <TableCell
-        className="font-medium"
-        style={{ textAlign: "center" }}
-      >
-        {(maintenanceCalories ?? 0) + 250}
-      </TableCell>
-      <TableCell style={{ textAlign: "center" }}>0.5</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell
-        className="font-medium"
-        style={{ textAlign: "center" }}
-      >
-        {(maintenanceCalories ?? 0) + 500}
-      </TableCell>
-      <TableCell style={{ textAlign: "center" }}>1.0</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell
-        className="font-medium"
-        style={{ textAlign: "center" }}
-      >
-        {(maintenanceCalories ?? 0) + 750}
-      </TableCell>
-      <TableCell style={{ textAlign: "center" }}>1.5</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell
-        className="font-medium"
-        style={{ textAlign: "center" }} 
-      >
-        {(maintenanceCalories ?? 0) + 1000}
-      </TableCell>
-      <TableCell style={{ textAlign: "center" }}>2.0</TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
-
+      <Card style={{marginTop: '10px' }}>
+        <CardHeader>
+          <CardTitle>{name}'s Calorie Intake</CardTitle>
+          <CardDescription>Muscle Gained / Week (lbs)</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+              <Line
+                dataKey="calories"
+                type="monotone"
+                stroke={chartConfig.calories.color}
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                dataKey="protein"
+                type="monotone"
+                stroke={chartConfig.protein.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter>
+          <div className="flex w-full items-start gap-2 text-sm">
+            <div className="grid gap-2">
+              <div className="flex items-center gap-2 font-medium leading-none">
+                Protein Intake per Calorie{" "}
+                <TrendingUp className="h-4 w-4" />
+              </div>
+              <div className="flex items-center gap-2 leading-none text-muted-foreground">
+                Add depicted calories to maintenance
+              </div>
             </div>
-        )}
-
-    </div>
+          </div>
+        </CardFooter>
+      </Card>
       <Accordion type="single" collapsible className="w-full text-[#44624a]">
       <AccordionItem value="item-1" className="border-b-[1px] border-[#44624a]">
         <AccordionTrigger>Monday</AccordionTrigger>
