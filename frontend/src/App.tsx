219014f21750683962dd63adee2725188e31a39d
NoNathan17 <nathanong1217@gmail.com>
2024-11-23 23:12:15 -0800

    removed register import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/login"
-import Register from "./pages/register"
 import Home from "./pages/home"
 import Profile from "./pages/profile"
 import Bodybuilding from "./pages/bodybuilding"
